9d4c0cd6297bd06b6f47f80d2aff6e2c851e9bbc
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-03-07 14:29:51 +0700

    Menambahkan Output dan Looping

--- 155_BubbleSort/155_BubbleSort.cpp
+++ 155_BubbleSort/155_BubbleSort.cpp
@@ -13,8 +13,20 @@ void input() {            //Procedure untuk input
 		else {                //Jika n lebih dari 20
 			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";    //Output ke layar
 		}
-
 	}
+
+	cout << endl;                                                //Output ke barisan kosong
+	cout << "=======================" << endl;                   //Output ke layar
+	cout << "Masukan Eleman Array" << endl;                      //Output ke layar
+	cout << "=======================" << endl;                   //Output ke layar
+
+	for (int i = 0; i < n; i++) {                   //Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke -" << (i + 1) << ": ";     //Output ke layar
+		cin >> a[i];                                //Input dari pengguna
+	}
+}
+
+
 
 
 		
